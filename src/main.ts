99c48032d33c1078dd2c2f5a68f4236cafbe26ad
Mar <mar@strawmelonjuice.com>
2023-10-27 00:52:14 +0200

    unmessified it lol

--- src/main.ts
+++ src/main.ts
@@ -4,157 +4,58 @@ import os from "os";
 import chalk from "chalk";
 import path, { dirname } from "path";
 import fs from "fs";
-const pkgjsn: { version: string, bananenVersion : string} = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json"), { encoding: "utf8", flag: "r" }).toString())
+const pkgjsn: { version: string, bananenVersion: string } = JSON.parse(fs.readFileSync(path.join(__dirname, "../package.json"), { encoding: "utf8", flag: "r" }).toString())
+import { execSync } from 'child_process';
+
+
 const KIVI_VERSION = pkgjsn.version;
-const BANANEN_VERSION = pkgjsn.bananenVersion;
+
 export class KiviBananen {
     bananenexecutable!: string;
-    execute: (command: string, callback: CallableFunction) => void;
     public info: { versions: { kivi: string; bananen: string; }; cwd: string; };
-    cwd: string;
     constructor(cwd: string) {
-        const exec = require("child_process").exec;
-        this.info = {
-            versions: {
-                kivi: KIVI_VERSION,
-                bananen: BANANEN_VERSION
-            },
-            cwd: cwd
+        const shell = (cmd: string) => execSync(cmd, { encoding: 'utf8' });
+
+         const executableIsAvailable = (name: string) => {
+            try { shell(`which ${name}`); return true }
+            catch (error) { return false }
         }
-        this.cwd = this.info.cwd;
-        this.execute = (command: string, callback: CallableFunction) => {
-            exec(
-                command,
-                {
-                    cwd: cwd,
+        if (executableIsAvailable("bananen")) {
+            this.info = {
+                versions: {
+                    kivi: KIVI_VERSION,
+                    bananen: execSync("bananen --minimal help version").toString().replace("\n", ""),
                 },
-                function (error: Error, stdout: string, stderr: string) {
-                    callback(stdout);
-                },
+                cwd: cwd
+            }
+            console.log(
+                `Successfully initialised ${chalk.bold(
+                    chalk.greenBright("Kivi! 🥝"),
+                )} ${chalk.italic("v" + KIVI_VERSION)} by ${chalk.redBright("Straw") +
+                chalk.green("melon") +
+                chalk.yellowBright("juice")
+                } ${chalk.magenta("Mar")}.\nusing Bananen version "${this.info.versions.bananen
+                }", also by ${chalk.redBright("Straw") +
+                chalk.green("melon") +
+                chalk.yellowBright("juice")
+                } ${chalk.magenta("Mar")}.`,
             );
+        } else {
+            this.info = {
+                versions: {
+                    kivi: KIVI_VERSION,
+                    bananen: "none",
+                },
+                cwd: cwd
+            }
+            console.error("KiviError: Bananen executable not on path.");
+            return;
         }
     }
-    do(callback: (KiviInstance: typeof this) => void): void {
-        const exec = require("child_process").exec;
-
-        exec(
-            "cargo --version",
-            {
-                cwd: process.cwd(),
-            },
-             async (b: Error, a: string, c: string) => {
-                try {
-                if (b == null) {
-                    console.log("Cargo: `" + a.replace(/(\r\n|\n|\r)/gm, "") + "` was found. Compiling Bananen from source!\n");
-                    const bananensourcedir = path.join(__dirname, "../bananen-source/");
-                    const bananensourcepack = path.join(bananensourcedir,`./packed-${BANANEN_VERSION}.tar.gz`);
-                    if (!fs.existsSync(bananensourcedir)) {
-                    fs.mkdirSync(bananensourcedir);
-                    }
-                    if (!fs.existsSync(bananensourcepack)) {
-                        const res = await axios.get(`https://github.com/strawmelonjuice/bananen/archive/refs/tags/v${BANANEN_VERSION}.tar.gz`, { responseType: 'arraybuffer' });
-                        fs.writeFileSync(bananensourcepack, res.data);
-                    };
-                    tar.extract({
-                        file: bananensourcepack,
-                        cwd: bananensourcedir,
-                        sync: true,
-                        strip: 1
-                    });
-                    exec(
-                        "cargo build -r",
-                        {
-                            cwd: bananensourcedir,
-                        },
-                        function (error: Error, stdout: string, stderr: string) {
-                            if (error !== null) throw new Error("Compile fail.");
-                        },
-                    );
-                    this.bananenexecutable = path.join(bananensourcedir,"./target/release/bananen");
-                    if (os.type().toLowerCase() == "windows_nt") {
-                        this.bananenexecutable = path.join(bananensourcedir, "./target/release/bananen.exe");
-                    }
-                } else { throw new Error("No cargo found. Using bundled Bananen executables!")}}
-                catch {
-                    let machine_double;
-                    switch (os.type().toLowerCase()) {
-                        case "windows_nt":
-                            switch (os.arch().toLowerCase()) {
-                                case "x64":
-                                    machine_double = "windows_64";
-                                    break;
-                                default:
-                                    throw new Error(
-                                        chalk.red("ERROR: ") +
-                                        "Unsupported processor architecture! Supported architectures on Windows are:\n- 64-bit (AMD64)",
-                                    );
-                                    // process.exit(1);
-                                    break;
-                            }
-                            break;
-                        case "linux":
-                            switch (os.arch().toLowerCase()) {
-                                case "x64":
-                                    machine_double = "linux_64";
-                                    break;
-                                case "arm":
-                                    machine_double = "linux_arm";
-                                    break;
-                                default:
-                                    throw new Error(
-                                        chalk.red("ERROR: ") +
-                                        "Unsupported processor architecture! Supported architectures on Linux are:\n- 64-bit (AMD64)\n- arm (aarch64)",
-                                    );
-                                    // process.exit(1);
-                                    break;
-                            }
-                            break;
-                        default:
-                            throw new Error(
-                                chalk.red("ERROR: ") +
-                                "Unsupported OS! Supported OS's are: \n- Windows\n- Linux",
-                            );
-                        // process.exit(1);
-                    }
-                    switch (machine_double) {
-                        case "windows_64":
-                            this.bananenexecutable = path.join(
-                                __dirname,
-                                "/../bundled/" +
-                                machine_double +
-                                "_" +
-                                BANANEN_VERSION +
-                                ".exe",
-                            );
-                            break;
-                        case "linux_64" || "linux_arm":
-                            this.bananenexecutable = path.join(
-                                __dirname,
-                                "/../bundled/" + machine_double + "_" + BANANEN_VERSION,
-                            );
-                            this.execute(`chmod +x '${this.bananenexecutable}'`, () => { console.log(`Successfully made '${this.bananenexecutable}' executable.`) })
-                            break;
-                        default:
-                            throw new Error(chalk.red("ERROR: ") + "Unsupported machine!");
-                            // process.exit(1);
-                            break;
-                    }
-                }
-                 console.log(
-                     `Seemingly successfully initialised ${chalk.bold(
-                         chalk.greenBright("Kivi! 🥝"),
-                     )} ${chalk.italic("v" + KIVI_VERSION)} by ${chalk.redBright("Straw") +
-                     chalk.green("melon") +
-                     chalk.yellowBright("juice")
-                     } ${chalk.magenta("Mar")}.\nusing Bananen binairy "${this.bananenexecutable
-                     }", also by ${chalk.redBright("Straw") +
-                     chalk.green("melon") +
-                     chalk.yellowBright("juice")
-                     } ${chalk.magenta("Mar")}.`,
-                 );
-                 callback(this);
-            },
-        );
+    private bananenRun(cmd: string) {
+        return execSync("bananen --minimal " + cmd, {
+            cwd: this.info.cwd,
+        }).toString();
     }
     add(type: number, breaking: boolean, change: string) {
         var cmd: string;
@@ -164,37 +65,25 @@ export class KiviBananen {
         else if (type === 3) ctype = "fix";
         else ctype = "removal";
         if (breaking)
-            cmd = `${this.bananenexecutable} add ${ctype} "${change}" --breaking`;
-        else cmd = `${this.bananenexecutable} add ${ctype} "${change}"`;
+            cmd = `add ${ctype} "${change}" --breaking`;
+        else cmd = `add ${ctype} "${change}"`;
         let cmmnd: string = cmd;
         // console.log(cmmnd);
-        this.execute(cmmnd, () => {});
+        return this.bananenRun(cmmnd);
     }
     init(force: boolean = false) {
         if (force)
-            this.execute(`${this.bananenexecutable} init --proceed`, () => {});
-        else this.execute(`${this.bananenexecutable} init`, () => {});
+            return this.bananenRun("init --proceed");
+        else return this.bananenRun("init");
     }
     dub(releasename: string) {
-        this.execute(`${this.bananenexecutable} dub ${releasename}`, () => {});
-}
+        return this.bananenRun(`dub ${releasename}`);
+    }
 
     regen() {
-        this.execute(`${this.bananenexecutable} regen`, () => {});
+        return this.bananenRun("regen");
     }
     chdir(cwd: string) {
-        this.cwd = cwd;
-        var exec = require("child_process").exec;
-        this.execute = (command: string, callback: CallableFunction) => {
-            exec(
-                command,
-                {
-                    cwd: cwd,
-                },
-                function (error: Error, stdout: string, stderr: string) {
-                    callback(stdout);
-                },
-            );
-        };
+        this.info.cwd = cwd;
     }
 }
--- src/test.ts
+++ src/test.ts
@@ -2,10 +2,6 @@ import { KiviBananen } from "./main";
 console.log("first")
 import path from 'path';
 const kivi = new KiviBananen(path.join(__dirname, "../yes/"));
-// console.log("second")
-// kivi.regen()
-
-kivi.do((kivi) => {
 console.log("second")
+kivi.init()
 kivi.regen()
-});
